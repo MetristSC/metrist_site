500ad0d8dc89ffb5b1ca24b2ab364555667c4464
Cees de Groot <casedeg@gmail.com>
2020-09-28 21:16:39 -0400

    Move secret key base back to compile time config

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,16 @@ config :metrist, Metrist.EventStore,
    hostname: "localhost",
    pool_size: 10
 
+# We have the shared secret key base in Github so all the builds
+# have the same one, independent of node, too. So we burn it into
+# the release at compile time.
+secret_key_base =
+  System.get_env("PHOENIX_SECRET_KEY_BASE") ||
+    raise """
+    environment variable PHOENIX_SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
 config :metrist, MetristWeb.Endpoint,
   http: [
     port: 4000,
@@ -26,7 +36,7 @@ config :metrist, MetristWeb.Endpoint,
     port: 80
   ],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: "see runtime config",
+  secret_key_base: secret_key_base,
   server: true
 
 config :logger, level: :info
--- config/releases.exs
+++ config/releases.exs
@@ -12,18 +12,8 @@ mepp =
     Environment variable METRIST_EVENTSTORE_PROD_PASSWORD is missing!
     """
 
-secret_key_base =
-  System.get_env("PHOENIX_SECRET_KEY_BASE") ||
-    raise """
-    environment variable PHOENIX_SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
-
 config :metrist, Metrist.Repo,
   password: mpp
 
 config :metrist, Metrist.EventStore,
   password: mepp
-
-config :metrist, MetristWeb.Endpoint,
-  secret_key_base: secret_key_base
