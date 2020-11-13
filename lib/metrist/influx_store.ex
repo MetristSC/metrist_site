bdc4020602b3e776d9e007f6eb521207396a3464
Cees de Groot <casedeg@gmail.com>
2020-11-13 14:09:30 -0500

    Adapt for simpler agent ping format

--- lib/metrist/influx_store.ex
+++ lib/metrist/influx_store.ex
@@ -3,22 +3,17 @@ defmodule Metrist.InfluxStore do
     otp_app: :metrist
 
   def write_from_agent(account_uuid, node_id, payload) do
-    points = Enum.map(payload, fn {measurement, points} ->
-      [timestamp, values_and_tags] = points
-      Enum.map(values_and_tags, fn [value, field_name, tags, unit, kind] ->
-        tags = Map.put(tags, "account_uuid", account_uuid)
-        tags = Map.put(tags, "node_id", node_id)
-        tags = if unit == "", do: tags, else: Map.put(tags, "unit", unit)
-        tags = Map.put(tags, "kind", kind)
-        %{
-          measurement: "#{account_uuid}/#{node_id}/#{measurement}",
-          fields: %{field_name => value},
-          tags: tags,
-          timestamp: timestamp
-        }
-      end)
+    IO.puts("Write #{inspect payload}")
+    points = Enum.map(payload, fn {measurement, [timestamp, fields, tags]} ->
+      tags = Map.put(tags, "account_uuid", account_uuid)
+      tags = Map.put(tags, "node_id", node_id)
+      %{
+        measurement: "#{account_uuid}/#{node_id}/#{measurement}",
+        fields: fields,
+        tags: tags,
+        timestamp: timestamp
+      }
     end)
-    points = List.flatten(points)
     write(%{points: points})
   end
 
