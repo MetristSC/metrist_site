281aa021d7c72adedffbff5c7631ce10f0b0ac0f
Cees de Groot <casedeg@gmail.com>
2020-11-18 16:52:07 -0500

    Add github oauth config to runtime config

--- config/releases.exs
+++ config/releases.exs
@@ -1,19 +1,23 @@
 import Config
 
-mpp =
-  System.get_env("METRIST_PROD_PASSWORD") ||
-    raise """
-    Environment variable METRIST_PROD_PASSWORD is missing!
-    """
+mpp = System.get_env("METRIST_PROD_PASSWORD") ||
+  raise "Environment variable METRIST_PROD_PASSWORD is missing!"
 
-mepp =
-  System.get_env("METRIST_EVENTSTORE_PROD_PASSWORD") ||
-    raise """
-    Environment variable METRIST_EVENTSTORE_PROD_PASSWORD is missing!
-    """
+mepp = System.get_env("METRIST_EVENTSTORE_PROD_PASSWORD") ||
+  raise "Environment variable METRIST_EVENTSTORE_PROD_PASSWORD is missing!"
+
+ghid = System.get_env("GITHUB_CLIENT_ID") ||
+  raise "Environment variable GITHUB_CLIENT_ID is missing!"
+
+ghsec = System.get_env("GITHUB_CLIENT_SECRET") ||
+  raise "Environment variable GITHUB_CLIENT_SECRET is missing!"
 
 config :metrist, Metrist.Repo,
   password: mpp
 
 config :metrist, Metrist.EventStore,
   password: mepp
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: ghid
+  client_secret: ghsec
